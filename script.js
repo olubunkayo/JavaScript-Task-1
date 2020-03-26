e73da1f3fbdfb4a4681f28d63ec777f36b9fba14
olubunkayo <tolu.olufotebi@gmail.com>
2020-03-26 22:54:42 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var name = 'Tolulope Olufotebi';
+console.log('My name is: + name');
+
+var course = [ 'html', 'css', 'javascript', 'design', 'c#' ];
+for (var i = 0; i <= 200; i + 2) {
+	course.push(i);
+}
